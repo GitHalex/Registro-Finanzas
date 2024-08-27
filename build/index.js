6c4c8eec8c02da782583be72162e657d32510dc0
GitHalex <calex1731@gmail.com>
2024-08-27 00:22:23 -0400

    Ejericico donde ingresamos los elelmentos de entrada

--- build/index.js
+++ build/index.js
@@ -1,1 +1,4 @@
 "use strict";
+const creditDebit = document.querySelector("#creditDebitSelect");
+const toFrom = document.querySelector("#toFromInput");
+console.log(toFrom.value);
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const creditDebit = document.querySelector(
+  "#creditDebitSelect"
+) as HTMLSelectElement;
+const toFrom = document.querySelector("#toFromInput") as HTMLInputElement;
+
+console.log(toFrom.value);
